1dc050d58ada52c6d16e5abd65476fe1e43111a2
Ian Brown <ianbrown112@gmail.com>
2017-11-22 18:58:24 -0700

    Created home and login pages, modified routes.py for these pages, added login-form.

--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import SignupForm
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///learningflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
 app.secret_key = "developmet-key"
@@ -28,10 +30,39 @@ def signup():
 			newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
 			db.session.add(newuser)
 			db.session.commit()
+			
+			#session is Flask app session, not db session
+			session['email'] = newuser.email
+			return redirect(url_for('home'))
+
 			return "Success!"
 
 	elif request.method == 'GET':
 		return render_template('signup.html', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	form = LoginForm()
+
+	if request.method == "POST":
+		if form.validate == False:
+			return render_template("login.html", form=form)
+		else:
+			email = form.email.data
+			password = form.password.data
+
+			user = User.query.filter_by(email=email).first()
+			if user is not None and user.check_password(password):
+				session['email'] = form.email.data
+				return redirect(url_for('home'))
+			else:
+				return redirect(url_for('login'))
+
+	elif request.method == 'GET':
+		return render_template('login.html', form=form)
+@app.route("/home")
+def home():
+	return render_template("home.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
